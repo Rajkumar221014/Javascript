7d49fae132cd68cfbd630ad4467e82225c437f9d
Rajkumar221014 <razkumarji27@gmail.com>
2025-03-11 10:28:28 +0000

    var const let

---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 6969
+let accountEmail = "raz@gmail.com"
+var accountPassword = "12345"
+accountCity="Delhi"
+
+accountEmail = "rajji@gmail.com"
+accountPassword = "9999"
+accountCity = "Japan"
+
+// accountId = 2
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
