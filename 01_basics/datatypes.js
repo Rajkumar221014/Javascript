993f0a9d68f3ff134e958f1d0d1a560f34c15fb6
Rajkumar221014 <razkumarji27@gmail.com>
2025-03-11 10:58:57 +0000

    data types

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code as a new version
+
+console.log(3 +      // code readability is important so avoid this syntax
+    3)
+    console.log("Raj")
+
+
+//datatypes in js are:-
+
+//numbers
+//bigint
+//string
+//boolean
+//null = "standalone value"
+//undefined
+//symbol = "unique"
+
+//object
+
+console.log(typeof undefined ); //type is undefined
+console.log(typeof null); // type is object 
